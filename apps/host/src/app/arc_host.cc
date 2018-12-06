0dd055593cbe1473ddc7b0d4b6162949a9cab4ac
eddiesasagawa <eddietio11@gmail.com>
2018-12-05 22:12:05 -0600

    Added new key callbacks for commanding main motors

--- apps/host/src/app/arc_host.cc
+++ apps/host/src/app/arc_host.cc
@@ -33,26 +33,39 @@ void ArcHost::Spin() {
   bool quit_now = false;
 
   /* register some callbacks to user interface */
-  user_if_.AddCallback('w', [&](){
+  user_if_.AddCallback('a', [&](){
+    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdUpperRotorUp);
+    logger_->debug("commanding upper rotor up.");
+    h2ai_.Send(&msg);
+  });
+  user_if_.AddCallback('z', [&](){
+    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdUpperRotorDown);
+    logger_->debug("commanding upper rotor down.");
+    h2ai_.Send(&msg);
+  });
+
+  user_if_.AddCallback('s', [&](){
+    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdLowerRotorUp);
+    logger_->debug("commanding lower rotor up.");
+    h2ai_.Send(&msg);
+  });
+  user_if_.AddCallback('x', [&](){
+    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdLowerRotorDown);
+    logger_->debug("commanding lower rotor down.");
+    h2ai_.Send(&msg);
+  });
+
+  user_if_.AddCallback('d', [&](){
     common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdPitchForward);
     logger_->debug("commanding pitch fwd.");
     h2ai_.Send(&msg);
   });
-  user_if_.AddCallback('s', [&](){
+  user_if_.AddCallback('c', [&](){
     common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdPitchBack);
     logger_->debug("commanding pitch back.");
     h2ai_.Send(&msg);
   });
-  user_if_.AddCallback('a', [&](){
-    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdTurnLeft);
-    logger_->debug("commanding left turn.");
-    h2ai_.Send(&msg);
-  });
-  user_if_.AddCallback('d', [&](){
-    common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdTurnRight);
-    logger_->debug("commanding right turn.");
-    h2ai_.Send(&msg);
-  });
+
   user_if_.AddCallback('q', [&](){
     common::AHICommandMessage msg(common::AHICommandMessage::kAhiCmdQuit);
     logger_->debug("commanding program termination.");
